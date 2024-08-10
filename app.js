e6ba889c01e63ed9ed1950e6fb6b654157883a4a
Deodate <ajadeo04@gmail.com>
2024-08-10 23:02:58 +0200

    Progress

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const pg = require('pg');
+
+require('dotenv').config();
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
